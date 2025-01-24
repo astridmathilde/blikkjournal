b8caae905ee43847f9d8b0c4eaba4f05e43f5879
astridmathilde <heia@astridmathilde.no>
2025-01-24 21:11:40 +0100

    ready to start building

---
+++ app/blikkjournal/page.js
@@ -0,0 +1,11 @@
+export default function Blikkjournal() {
+  return (
+    <>
+      <h2>Hva er en blikkjournal?</h2>
+      <p>En <em>blikkjournal</em> er som en dagbok, bare med bilder. En samling bilder av ting jeg har sett, opplevd eller lagt merke til, noe som fanget oppmerksomheten min der og da. I stedet for å dele det på sosiale medier, deler jeg det her. </p>
+
+      <h2>Hvorfor blikkjournal?</h2>
+      <p>Blikkjournalen fungerer som en motvekt til sosiale medier, følgere og likerklikk. Som en analog bildebok, ment for de som vil se.</p>
+    </>
+  );
+}
