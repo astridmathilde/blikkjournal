7e65f2c11fb3f677bdb88c5e5c95e877b5a30e63
astridmathilde <heia@astridmathilde.no>
2025-02-24 01:52:57 +0100

    ☁️

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,13 @@
-import Link from "next/link";
+
 import '../assets/scss/global.scss';
-
-export const siteTitle = "astrid's blikkjournal";
-
-export const metadata = {
-  title: siteTitle,
-  description: "a collection of moments",
-};
 
 export default async function RootLayout({ children }) {
   return (
     <html lang="en">
     <body>
-    <header>
-    <h1><Link href="/">blikkjournal</Link></h1>
-    <p>collected moments</p>
-    </header>
     <main>
     {children}
     </main>
-    <footer>
-    <p>© <a href="https://astridmathilde.no" target="_blank" rel="external">Astrid Mathilde</a> {(new Date().getFullYear())}</p>
-    <p><Link href="#">Back to top</Link></p>
-    </footer>
     </body>
     </html>
   );
