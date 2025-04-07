978a5f3ea0f6248bccfebc1fee5636f947b6ea1f
astridmathilde <heia@astridmathilde.no>
2025-04-07 10:15:48 +0200

    added colophon page

---
+++ app/colophon/page.js
@@ -0,0 +1,11 @@
+import styles from "../../assets/scss/colophon.module.scss";
+
+export default function colophon() {
+  return (
+    <>
+      <div className={styles.content}>
+        <p>hei</p>
+      </div>
+    </>
+  )
+}
