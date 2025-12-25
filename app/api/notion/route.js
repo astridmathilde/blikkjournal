7fd5b17954397101cba9cfe9cc40431f1db43884
astridmathilde <heia@astridmathilde.no>
2025-12-26 00:41:01 +0100

    testing my notion webhook

--- app/api/notion/route.js
+++ app/api/notion/route.js
@@ -2,28 +2,33 @@
 * Revalidating the cache when posts are updated, using Notion API Webhooks
 * @url https://developers.notion.com/reference/webhooks
 */
+import { revalidateTag } from 'next/cache';
 import { NextResponse } from 'next/server';
+import { createHmac, timingSafeEqual } from "crypto"
 
 export async function POST(request) {
   try {
     const response = JSON.parse(await request.text());
     
-    if (response.verification_token) {
-      console.log(response.verification_token);
+    /* receiving things from the webhook here */
+    if (response) {
+      console.log(response);
       return NextResponse.json({ 
         message: 'hei:)' 
       });
     }
-      return NextResponse.json({ 
-        received: true,
-        event_type: response.type 
-      });
-      
-    } catch (error) {
-      console.error(error);
-      return NextResponse.json({ 
-        error: 'neeei:(',
-        message: error.message 
-      }, { status: 500 });
-    }
+    return NextResponse.json({ 
+      received: true,
+      event_type: response.type 
+    });
+  } 
+
+  /* error handling stuff */
+  catch (error) {
+    console.error(error);
+    return NextResponse.json({ 
+      error: 'neeei:(',
+      message: error.message 
+    }, { status: 500 });
   }
+}
