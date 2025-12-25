b2ca940707be02f4b4d3b3a843ff9db6d2c91e00
astridmathilde <heia@astridmathilde.no>
2025-12-26 00:16:22 +0100

    adding webhooks from notion api

--- app/api/notion/page.js
+++ app/api/notion/page.js
@@ -1,25 +1,9 @@
 /**
 * Revalidating the cache when posts are updated, using Notion API Webhooks
-
 * @url https://developers.notion.com/reference/webhooks
 */
 
-import { NextResponse } from 'next/server';
+export default function notion() {
+    console.error("hei:)");
 
-export async function GET(request) {
-  const response = JSON.parse(await request.text());
-  
-  try {
-    console.log(response);
-    return NextResponse.json({ 
-      message: 'hei:)' 
-    });
-  }
-  
-  catch (error) {
-    console.error(error);
-    return NextResponse.json({ 
-      message: 'neeei:(' 
-    });
-  }
 }
