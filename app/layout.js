dd4a7b1d957a6f404886794d9c78a415eab80806
astridmathilde <heia@astridmathilde.no>
2025-12-25 17:51:08 +0100

    hello new design

--- app/layout.js
+++ app/layout.js
@@ -1,47 +1,17 @@
 import Link from "next/link";
-import localFont from 'next/font/local';
 import '../assets/scss/global.scss';
 import styles from '../assets/scss/layout.module.scss'
 import { getDatabase } from "/lib/notion";
 
-export const siteTitle = "blikkjournal";
-export const siteDescription = "a collection of things and thoughts";
+export const siteTitle = "blikk.directory";
+export const siteDescription = "a collection of things and thoughts and everyday observations";
 
 export const metadata = {
-  metadataBase: new URL('https://astrid.observer'),
+  metadataBase: new URL('https://blikk.directory'),
   title: siteTitle,
   description: siteDescription,
 };
 
-const NyghtSerif = localFont({
-  src: [
-    {
-      path: '../assets/fonts/NyghtSerif-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    }
-  ]
-});
-
-const Ronzino = localFont({
-  src: [
-    {
-      path: '../assets/fonts/Ronzino-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/Ronzino-Oblique.woff2',
-      weight: '400',
-      style: 'italic',
-    },
-    {
-      path: '../assets/fonts/Ronzino-Bold.woff2',
-      weight: '600',
-      style: 'normal',
-    },
-  ]
-});
 
 async function displayProperties() {
   const data = await getDatabase();
@@ -57,8 +27,9 @@ export default async function RootLayout({ children }) {
     <body>
     <div className={styles.wrapper}>
     <header className={styles.header}>
-    <h1 className={NyghtSerif.className + " " + styles.title}><Link href="/">{siteTitle}</Link></h1>
-    <nav className={Ronzino.className + " " + styles.categories} id="categories">
+    <h1 className={styles.title}><Link href="/">{siteTitle}</Link></h1>
+    <p className={styles.description}>{siteDescription}</p>
+    <nav className={styles.categories} id="categories">
     <ul>
     <li key="all"><Link href="/">all</Link></li>
     {menuItems.map((item) => (
@@ -67,11 +38,11 @@ export default async function RootLayout({ children }) {
     </ul>
     </nav>
     </header>
-    <main className={Ronzino.className + " " + styles.content}>
+    <main className={styles.content}>
     {children}
     </main>
-    <footer className={Ronzino.className + " " + styles.footer}>
-    <p className={styles.colophon}><span className={styles.author}>© <a href="https://astridmathilde.no" target="_blank" rel="external">Astrid Mathilde</a></span> <span className={styles.period}>2018–{(new Date().getFullYear())}</span></p>
+    <footer className={styles.footer}>
+    <p className={styles.colophon}><span className={styles.author}>© <a href="https://astridmathilde.no" target="_blank" rel="external">Astrid Boberg</a></span> <span className={styles.period}>2018–{(new Date().getFullYear())}</span></p>
     <p className={styles.backtotop}><Link href="#">Back to top</Link></p>
     </footer>
     </div>
