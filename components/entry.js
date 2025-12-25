e2db30ab243d00563821c1cb7c9614e5c526bfaf
astridmathilde <heia@astridmathilde.no>
2025-12-25 22:42:54 +0100

    better fetching of images

--- components/entry.js
+++ components/entry.js
@@ -1,5 +1,5 @@
+import Image from 'next/image';
 import styles from '../assets/scss/entry.module.scss';
-import EntryImage from './entry-image';
 
 export default function Entry(entry) {
   const entryId = entry.id;
@@ -30,7 +30,7 @@ export default function Entry(entry) {
     </ul>
     </div>
     <figure>
-    <EntryImage src={imgUrl} alt={title} entryId={entryId}/>
+    <Image src={imgUrl} alt={title} fill={true}  />
     </figure> 
     </article>
   );
