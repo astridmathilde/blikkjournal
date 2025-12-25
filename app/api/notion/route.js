0aa7b98fecbd8aa1c2aa1f91a246b28f788c4c73
astridmathilde <heia@astridmathilde.no>
2025-12-26 00:52:15 +0100

    testing my notion webhook

--- app/api/notion/route.js
+++ app/api/notion/route.js
@@ -4,31 +4,17 @@
 */
 import { revalidateTag } from 'next/cache';
 import { NextResponse } from 'next/server';
-import { createHmac, timingSafeEqual } from "crypto"
 
 export async function POST(request) {
   try {
     const response = JSON.parse(await request.text());
-    
     /* receiving things from the webhook here */
     if (response) {
       console.log(response);
-      return NextResponse.json({ 
-        message: 'hei:)' 
-      });
     }
-    return NextResponse.json({ 
-      received: true,
-      event_type: response.type 
-    });
-  } 
-
+  }
   /* error handling stuff */
   catch (error) {
     console.error(error);
-    return NextResponse.json({ 
-      error: 'neeei:(',
-      message: error.message 
-    }, { status: 500 });
   }
 }
