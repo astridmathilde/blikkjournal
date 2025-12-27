1f76d4ce7887c7e70d6242bf4562adb77167941a
astridmathilde <heia@astridmathilde.no>
2025-12-27 22:52:11 +0100

    image fix

--- app/page.js
+++ app/page.js
@@ -14,14 +14,14 @@ export default async function Index() {
     <div key="entries">
     {entries.map((entry) => {      
       const entryId = entry.id;
-      const imgUrl = entry.properties.Image.files[0]?.file.url;
+      const proxySrc = `/api/images/${entryId}`;
       const title = entry.properties?.Title?.title[0]?.plain_text || "";
       const place = entry.properties?.Place?.select?.name || "";
       const city = entry.properties?.City?.select?.name || "";
       const country = entry.properties?.Country?.select?.name || "";
       const time = entry.properties.Time.date?.start;
       
-      return <Entry key={entryId} id={entryId} place={place} img={imgUrl} title={title} city={city} country={country} time={time} />
+      return <Entry key={entryId} id={entryId} place={place} img={proxySrc} title={title} city={city} country={country} time={time} />
     })}
     </div>
   );
