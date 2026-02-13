760cfa34f024cc753ae1c5156dac8497d7269d02
astridmathilde <heia@astridmathilde.no>
2026-02-13 13:41:15 +0100

    specified usage of generative ai

--- app/(views)/layout.js
+++ app/(views)/layout.js
@@ -53,7 +53,17 @@ export default function RootLayout({ children }) {
     
     <footer className={styles.footer}>
     <ColophonLink />
-    <ColophonContent />
+    <ColophonContent>
+
+    <h2>Colophon</h2>
+    <p>The site is coded by hand using the React framework <a href="https://nextjs.org" target="_blank" rel="external">Next.js</a>. Generative AI has not been used except for research and solving wicked problems. In those cases, the language model has been run locally using the application <a href="https://osaurus.ai" rel="external" target="_blank">Osaurus</a>, minimizing carbon emissions and resource usage.</p>
+    
+    <p>The photos are stored in <a href="https://notion.com" target="_blank" rel="external">Notion</a> and retrieved using <a href="https://developers.notion.com" target="_blank" rel="external">Notion API</a>. If you are curious about how everything is built, the code repository is public on <a href="https://github.com/astridmathilde/blikkjournal" target="_blank" rel="external">Github</a>.</p>
+    
+    <p>The typeface is <a href="https://www.collletttivo.it/typefaces/ronzino" target="_blank" rel="external">Ronzino</a> from <a href="https://www.collletttivo.it" target="_blank" rel="external">Collletttivo</a>. The icons are from <a href="https://ikonate.com" target="_blank" rel="external">Iconate</a>.</p>
+    
+    <p>© <a href="https://astridmathilde.no" target="_blank" rel="external">Astrid Boberg</a> 2018–{(new Date().getFullYear())}</p>
+    </ColophonContent>
     </footer>
   
     </body>
