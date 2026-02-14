207ac23ffe63ecfcaf165b41587cb125f9a82f39
astridmathilde <heia@astridmathilde.no>
2026-02-14 13:09:23 +0100

    fixed issue after restructuring

--- app/(views)/page.js
+++ app/(views)/page.js
@@ -1,32 +1,59 @@
-/* Get data from Notion */
 import { getEntries } from "../lib/notion";
-import GalleryEntry from "../components/entry/gallery";
-import styles from '../assets/scss/views/gallery.module.scss'
+import { siteTitle, siteDescription } from "../layout";
+import ListEntry from "../components/entry/index";
+import styles from "../assets/scss/views/index.module.scss";
 
-/* Display content */
-export default async function Gallery() {
+export const metadata = {
+metadataBase: new URL('https://blikk.directory'),
+title: siteTitle,
+description: siteDescription,
+};
+
+
+export default async function Index() {
   const entries = await getEntries();
   
   return (
     <>
-    <div key="entries" className={styles.gallery}>
+    <table className={styles.list}>
+    <thead>
+    <tr>
+    <th id="image">Image</th>
+    <th id="description">Description</th>
+    <th id="location">Location</th>
+    <th id="city">City, country</th>
+    <th id="category">Category</th>
+    <th id="camera">Camera</th>
+    <th id="date">Date</th>
+    </tr>
+    </thead>
+    <tbody>
     {entries.map((entry, index) => { 
-      const isAboveTheFold = index < 10;
-      
+      const isAboveTheFold = index < 15;   
+
       const entryId = entry.id;
       const title = entry.properties?.Title?.title[0]?.plain_text || "";
-      const name = entry.properties?.Image?.files[0]?.name || "";
-      const place = entry.properties?.Place?.select?.name || "";
+      const fileName = entry.properties?.Image?.files[0]?.name || "";
+      const location = entry.properties?.Place?.select?.name || "";
       const city = entry.properties?.City?.select?.name || "";
       const country = entry.properties?.Country?.select?.name || "";
-      const camera = entry.properties?.Camera?.select?.name || "";
       const time = entry.properties.Time.date?.start;
+      const camera = entry.properties?.Camera?.select?.name || "";
+      const category = entry.properties?.Category?.select?.name || "";  
       
-      return <GalleryEntry key={entryId} id={entryId} place={place} title={title} city={city} country={country} time={time} camera={camera} name={name} priority={isAboveTheFold ? "true" : ""} />
+      const date = new Date(time).toLocaleString(
+        'en-US',
+        {
+          month: 'short',
+          day: '2-digit',
+          year: 'numeric',
+        },
+      );   
       
+      return <ListEntry key={entryId} id={entryId} title={title} fileName={fileName} location={location} city={city} country={country} category={category} camera={camera} date={date} priority={isAboveTheFold ? "true" : ""}/>
     })}
-    </div>
+    </tbody>
+    </table>
     </>
-  );
-  
+  )
 }
