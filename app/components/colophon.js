ccc056a3d837b5df8421ce1fc0be0381ddd96153
astridmathilde <heia@astridmathilde.no>
2026-02-15 17:14:43 +0100

    dynamically rendering colophon

--- app/components/colophon.js
+++ app/components/colophon.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Draggable from "./draggable"
 import contentStyle from "../assets/scss/components/colophon/content.module.scss";
 import linkStyle from "../assets/scss/components/colophon/link.module.scss";
@@ -8,19 +8,7 @@ import IconClose from "./icons/icon-close";
 
 export default function Colophon({button, children}) {
   const [isOpen, makeOpen] = useState(false);
-  
-  useEffect(() => {
-    const colophon = document.getElementById("colophon");
-    
-    if (colophon) {
-      if (isOpen) {
-        colophon.style.display = "block";
-      } else {
-        colophon.style.display = "none";
-      }
-    }
-  }, [isOpen]);
-  
+
   const handleClick = () => {
     makeOpen(!isOpen);
   }
@@ -28,13 +16,14 @@ export default function Colophon({button, children}) {
   return (
     <>
     <button className={linkStyle.link} onClick={handleClick} aria-label={isOpen ? "close colophon" : "open colophon"}>{button}</button>
-    
-    <Draggable>
-    <div id="colophon" className={contentStyle.content}>
-    <button className={contentStyle.close} onClick={handleClick} aria-label={isOpen ? "close colophon" : ""}><IconClose /></button>
-    {children}
-    </div>
-    </Draggable>
+    {isOpen ? (
+      <Draggable>
+      <div id="colophon" className={contentStyle.content}>
+      <button className={contentStyle.close} onClick={handleClick} aria-label={isOpen ? "close colophon" : ""}><IconClose /></button>
+      {children}
+      </div>
+      </Draggable>
+    ) : "" }
     </>
   )
   
