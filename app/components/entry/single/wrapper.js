d0e276693683a1f70883061e4292450eb3ba5f7e
astridmathilde <heia@astridmathilde.no>
2026-03-02 22:23:55 +0100

    changed library for getting color from images

--- app/components/entry/single/wrapper.js
+++ app/components/entry/single/wrapper.js
@@ -1,35 +1,51 @@
 "use client";
 
-import { useEffect } from "react";
-import { useExtractColors } from "react-extract-colors";
+import { useEffect, useState } from "react";
+import { FastAverageColor } from "fast-average-color";
 import styles from "../../../assets/scss/components/entry/single/wrapper.module.scss";
 
 export default function SingleEntry({entryId, children}) {
-  const image =  `/api/images/${entryId}`;
-  const {lighterColor, loading} = useExtractColors(image, {
-    format: "rgb",
-    crossOrigin: "anonymous"
-  });
-
-  const opacity = 0.2;
-  const imageColor = lighterColor ? lighterColor : "";
-  const adjustedColor = imageColor.replace('rgb', 'rgba').replace(')', `, ${opacity})`);
-  
+  const [bgColor, setBgColor] = useState();
+  const image =  `/api/images/${entryId}`;  
   
   useEffect(() => {
-    if (adjustedColor && !loading) {
-      document.body.style.backgroundColor = adjustedColor;
-      
-      let metaThemeColor = document.querySelector('meta[name="theme-color"]');
-      if (!metaThemeColor) {
-        metaThemeColor = document.createElement('meta');
-        metaThemeColor.name = 'theme-color';
-        document.head.appendChild(metaThemeColor);
-      }
-      metaThemeColor.content = adjustedColor;
+    const fac = new FastAverageColor();
+    const img = new Image();
+    
+    img.onload = () => {
+      fac.getColorAsync(img, {algorithm: 'simple'}).then(color => {
+        const opacity = 0.2;
+        const adjustedColor = color.rgba.replace(/[\d.]+\)$/g, `${opacity})`);
+        
+        setBgColor(adjustedColor);
+        document.body.style.backgroundColor = adjustedColor;
+        
+        
+        let metaThemeColor = document.querySelector('meta[name="theme-color"]');
+        if (!metaThemeColor) {
+          metaThemeColor = document.createElement('meta');
+          metaThemeColor.name = 'theme-color';
+          document.head.appendChild(metaThemeColor);
+        }
+        metaThemeColor.content = adjustedColor;
+        console.log('EntryId:', entryId, 'Color:', adjustedColor);
+      })
+      .catch(error => {
+        console.log('oh no:', error);
+      })
     }
-  }, [adjustedColor, loading]);
-
+    
+    img.onerror = () => {
+      console.error('oh noooo:', image);
+    }
+    
+    img.src = image;
+    
+    return() => {
+      fac.destroy();
+    }
+  }, [image]);
+  
   return (
     <article className={styles.singleEntry}>
     {children}
