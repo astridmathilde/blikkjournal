f1014e0b13d806b696dea5f1125b951e0fa29103
astridmathilde <heia@astridmathilde.no>
2026-03-15 16:31:33 +0100

    updated notion api version

--- app/lib/notion.js
+++ app/lib/notion.js
@@ -7,6 +7,7 @@ import { Client } from "@notionhq/client";
 
 const notion = new Client({
   auth: process.env.NOTION_KEY,
+  notionVersion: "2025-09-03",
 });
 
 const databaseId = process.env.NOTION_DATABASE_ID;
@@ -14,41 +15,54 @@ const databaseId = process.env.NOTION_DATABASE_ID;
 /* Get database properties */
 export const getDatabase = unstable_cache(
   async () => {
-    const response = await notion.databases.retrieve({
-      database_id: databaseId
+    const response = await notion.request({
+      method: "get",
+      path: `databases/${databaseId}`,
     });
     return response;
   },
   ['properties'],
-  { revalidate: 2592000, tags: ['properties'] } // refresh every month
+  { revalidate: 2592000, tags: ['properties'] }
 );
+
+const getDataSourceId = async () => {
+  const db = await getDatabase();
+  const dataSource = db.data_sources?.[0];
+  return dataSource.id;
+};
 
 /* Get entries */
 export const getEntries = unstable_cache(
-  async() => {
-    const response = await notion.databases.query({
-      database_id: databaseId,
-      sorts: [
-        {
-          property: "Time",
-          direction: "descending"
-        }
-      ]
+  async () => {
+    const dataSourceId = await getDataSourceId();
+
+    const response = await notion.request({
+      method: "post",
+      path: `data_sources/${dataSourceId}/query`,
+      body: {
+        sorts: [
+          {
+            property: "Time",
+            direction: "descending",
+          },
+        ],
+      },
     });
+
     return response.results;
   },
   ['entries'],
-  { revalidate: 2592000, tags: ['entries']} // refresh every month
+  { revalidate: 2592000, tags: ['entries'] }
 );
 
 /* Get a single entry */
 export const getEntry = unstable_cache(
   async (pageId) => {
     const response = await notion.pages.retrieve({
-      page_id: pageId
+      page_id: pageId,
     });
     return response;
   },
   ['singleEntry'],
-  { revalidate: 2592000, tags: ['singleEntry'] } // refresh every month
+  { revalidate: 2592000, tags: ['singleEntry'] }
 );
