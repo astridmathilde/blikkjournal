0857f94260057c9077564620e62961dd36f7f03f
astridmathilde <heia@astridmathilde.no>
2026-04-03 00:16:39 +0200

    caching issue might be fixed

--- src/api/notion/route.js
+++ src/api/notion/route.js
@@ -8,14 +8,20 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request) {
   try {
-    const parsedRequest = JSON.parse(await parsedRequest.text());
+    const parsedRequest = JSON.parse(await request.text());
     
     /* revalidating the cache if there is any changes */
-    if (parsedRequest.type === 'page.properties_updated' || parsedRequest.type === 'page.created' || parsedRequest.type === 'page.deleted' || parsedRequest.type === 'page.undeleted') {
-      revalidateTag('entries', 'max');
-      revalidateTag('singleEntry', 'max');
-      revalidateTag('properties', 'max');
-      revalidateTag('entryColor', 'max');
+    if (
+      parsedRequest.type === 'page.properties_updated' ||
+      parsedRequest.type === 'page.created' ||
+      parsedRequest.type === 'page.deleted' ||
+      parsedRequest.type === 'page.undeleted'
+    ) {
+      revalidateTag('entries');
+      revalidateTag('singleEntry');
+      revalidateTag('allEntries');
+      revalidateTag('properties');
+      revalidateTag('entryColor');
       console.log('cache revalidated for', parsedRequest.type);
     }
     
