576915a7cde59a40e62379ce3fa6b5586597395e
astridmathilde <heia@astridmathilde.no>
2026-04-03 00:01:01 +0200

    renamed menu item index to list

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -11,7 +11,7 @@ export default function NavLink() {
     <nav className={styles.navigation} id="navigation">
     <ul>
     <li key="gallery"><Link href="/" className={styles.link + " " + `${pathname === '/' ? `${styles.active}` : ''}`}>Gallery</Link></li>
-    <li key="index"><Link href="/list" className={styles.link + " " + `${pathname === '/list' ? `${styles.active}` : ''}`}>Index</Link></li>
+    <li key="index"><Link href="/list" className={styles.link + " " + `${pathname === '/list' ? `${styles.active}` : ''}`}>List</Link></li>
     </ul>
     </nav>
   )
