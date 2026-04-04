730663210e34ed708623b71e4ae455ec5fc1b6a5
astridmathilde <heia@astridmathilde.no>
2026-04-05 00:18:50 +0200

    added clutter control for filter

--- src/components/filter/wrapper.js
+++ src/components/filter/wrapper.js
@@ -1,25 +1,32 @@
 /**
- * Used for wrapping content to display loading states when filter is changed
- */
+* Used for wrapping content to display loading states when filter is changed
+*/
 
 "use client";
 
 import { useTransition } from "react";
+import { useClutter } from "../clutter/context";
 import Loading from "../loading";
 import Filter from "./index";
 
 export default function FilterWrapper({ categories, locations, years, children }) {
-  const [isPending, startTransition] = useTransition();
-
-  return (
-    <>
-    <Filter
+  const { level } = useClutter();
+  
+  if (level <= -6) {
+    return children;
+  } else {
+    const [isPending, startTransition] = useTransition();
+    
+    return (
+      <>
+      <Filter
       categories={categories}
       locations={locations}
       years={years}
       startTransition={startTransition}
-    />
-    {isPending  ? <Loading /> : children }
-    </>
-  );
+      />
+      {isPending  ? <Loading /> : children }
+      </>
+    );
+  }
 }
